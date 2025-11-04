ad0c7ffc94eebf0c557cb222eb0165ffabb5a2e0
yuri_naiz <naiz.yuri@escola.pr.gov.br>
2025-11-04 09:14:14 -0300

    04/11

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botcaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
+function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavrasChave.join(", ")
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
 
     let frequencias = {};
 
-    for (let i of palavras){
-        frequencias[i]=0;
+    for (let i of palavras) {
+        frequencias[i] = 0;
 
-        for (let j of palavras){
-            if (i == j){
+        for (let j of palavras) {
+            if (i == j) {
                 frequencias[i]++;
             }
         }
     }
 
     return palavras;
+
+
+
+
 }
+
